ef09815e8855cf62de69beaae8c5f0f6d178eb90
maeldiy <maeldiy@gmail.com>
2013-10-01 22:20:02 +0200

    create daemon
    remove GUI from server

---
+++ src/Phonedoor_daemon.java
@@ -0,0 +1,66 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.net.*;
+import java.io.*;
+
+public class Phonedoor_daemon{
+  Thread thread;
+  DatagramSocket socket;
+
+  public static void main(String[] args) {
+	  Phonedoor_daemon u = new Phonedoor_daemon();
+  }
+  public Phonedoor_daemon(){
+	  new StartThread();
+  }
+  public class StartThread implements Runnable{
+  StartThread(){
+  thread = new Thread(this);
+  thread.start();
+
+  }
+  public void run(){
+  try{
+	  byte[] buffer = new byte[1024];
+	  int port = 5650;
+	  try{
+		  socket = new DatagramSocket(port);
+		  while(true){
+			  try{
+  //Receive request from client
+				  DatagramPacket packet =  
+						  new DatagramPacket(buffer, buffer.length );
+				  socket.receive(packet);
+				  InetAddress client = packet.getAddress();
+				  int client_port = packet.getPort();
+				  String donnees_recues = new String(buffer);
+				  if (donnees_recues.contains("end_request_calls")) {
+					  // Sending acknowledge
+					  String Answer = new String("OKFROMTAB"); 
+					  byte answer[] = Answer.getBytes();
+					  packet = 
+							  new DatagramPacket(answer, answer.length,client,client_port);
+					  socket.send(packet);
+	  // Launching apps, i.e executing task ;-)
+					  Runtime runtime = Runtime.getRuntime();
+	  // runtime.exec(new String[] { "wmplayer //play //close K:\\mp3\\doorbell.mp3" } );
+	//  runtime.exec(new String[] { "C:\\Program Files\\Windows Media Player\\wmplayer K:\\mp3\\doorbell.mp3" } );
+	  //runtime.exec(new String[] { "play K:\\mp3\\doorbell.mp3" } );
+					  runtime.exec(new String[] { "C:\\Users\\Loule\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe -profile-directory=\"Profile 1\""} ); // TO DO : define good user
+	  //UDPClient answer = new UDPClient(String.valueOf(client),String.valueOf(client_port), "ok_received");
+  	}
+  
+  
+  }
+  catch(UnknownHostException ue){}
+  }
+  }
+  catch(java.net.BindException b){}
+  }
+  catch (IOException e){
+  System.err.println(e);
+  }
+  }
+  }
+}
--- src/UDPServer.java
+++ src/UDPServer.java
@@ -1,8 +1,14 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
-import java.net.*;
-import java.io.*;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 public class UDPServer{
   JFrame frame;
@@ -13,12 +19,13 @@ public class UDPServer{
   Thread thread;
   DatagramSocket socket;
 
-  public static void main(String[] args) {
+/*  public static void main(String[] args) {
   UDPServer u = new UDPServer();
   }
+  */
   public UDPServer(){
 	  
-  frame = new JFrame("Text Server");
+ /* frame = new JFrame("Text Server");
   frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
   frame.setUndecorated(true);
   frame.getRootPane()
@@ -54,7 +61,8 @@ public class UDPServer{
   frame.add(panel);
   frame.setSize(400, 400);
   frame.setVisible(true);
-  new StartThread();
+  */
+	  new StartThread();
   }
   public class StartThread implements Runnable{
   StartThread(){
@@ -70,21 +78,33 @@ public class UDPServer{
   socket = new DatagramSocket(port);
   while(true){
   try{
-  area.append("Server is started\n");
+  //area.append("Server is started\n");
   //Receive request from client
   DatagramPacket packet =  
   new DatagramPacket(buffer, buffer.length );
   socket.receive(packet);
   InetAddress client = packet.getAddress();
   int client_port = packet.getPort();
-  area.append(" Received "
-  +new String(buffer)+" from "+client);
-  String Answer = new String("ok_received"); 
-  byte answer[] = Answer.getBytes();
-  packet = 
-  new DatagramPacket(answer, answer.length,client,client_port);
-  socket.send(packet);
-  //UDPClient answer = new UDPClient(String.valueOf(client),String.valueOf(client_port), "ok_received");
+  //area.append(" Received "
+  //+new String(buffer)+" from "+client);
+  String donnees_recues = new String(buffer);
+
+  if (donnees_recues.contains("end_request_calls")) {
+  // Sending acknowledge
+	  String Answer = new String("OKFROMTAB"); 
+	  byte answer[] = Answer.getBytes();
+	  packet = 
+			  new DatagramPacket(answer, answer.length,client,client_port);
+	  socket.send(packet);
+	  // Launching apps, i.e executing task ;-)
+	  Runtime runtime = Runtime.getRuntime();
+
+//	  runtime.exec(new String[] { "C:\\Program Files\\Windows Media Player\\wmplayer K:\\mp3\\doorbell.mp3" } );
+	
+	  runtime.exec(new String[] { "C:\\Users\\Loule\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe -profile-directory=\"Profile 1\""} ); // TO DO : define good user
+	  //UDPClient answer = new UDPClient(String.valueOf(client),String.valueOf(client_port), "ok_received");
+  	}
+  
   
   }
   catch(UnknownHostException ue){}
