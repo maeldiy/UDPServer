db2100bfa8b8e7a2cbccb7c32e4bc596467caf26
maeldiy <maeldiy@gmail.com>
2013-10-12 21:07:59 +0200

    stable version with config file reading
    missing GUI.jar

--- src/Phonedoor_daemon.java
+++ src/Phonedoor_daemon.java
@@ -5,8 +5,15 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.util.List;
 
-import sun.audio.*;
+import javax.swing.JOptionPane;
+
+import sun.audio.AudioPlayer;
+import sun.audio.AudioStream;
 
 
 public class Phonedoor_daemon{
@@ -47,63 +54,59 @@ public class Phonedoor_daemon{
 					  packet = 
 							  new DatagramPacket(answer, answer.length,client,client_port);
 					  socket.send(packet);
-					  // launch sound player
+		  // Determining OS Launching GUI 		
+				//	  Runtime runtime = Runtime.getRuntime();
+					  int OS = PlatformDetector.detect();
+					    try {
+					        if (OS == PlatformDetector.WINDOWS)
+					        	Runtime.getRuntime().exec(new String[] {"java -jar GUI.jar"});
+					        else { // assume Unix or Linux
+					        Runtime.getRuntime().exec(new String[] { "wmplayer //play //close K:\\mp3\\doorbell.mp3" } );
+					        }
+					      } catch (Exception e) {
+					        JOptionPane.showMessageDialog(null,
+					             ":\n" + e.getLocalizedMessage());
+					      }		
+					    // launch sound player
 					  try {
-						   // open the sound file as a Java input stream
-						    String gongFile = "K:\\mp3\\70\\CUBA.mp3";
+						  //for test : read parameters from text file 
+			        		final List<String> lignes = Files.readAllLines(FileSystems.getDefault().getPath("C:\\Phone_door\\config.txt"), StandardCharsets.ISO_8859_1);  //iso 8859 for windows , utf-8 for linux
+			        		String audiofilepath = lignes.get(1);
+			        		System.out.println("bla bla" + audiofilepath);
+						  
+						  // open the sound file as a Java input stream
+						   	String gongFile = audiofilepath;//"K:\\mp3\\70\\CUBA.mp3";
+						   	System.out.println(gongFile);
 						    InputStream in = new FileInputStream(gongFile);
-
 						    // create an audiostream from the inputstream
 						    AudioStream audioStream = new AudioStream(in);
-
 						    // play the audio clip with the audioplayer class
 						    AudioPlayer.player.start(audioStream);
-						    
-						    
-						 
-						/*    
-						   AudioInputStream stream;
-						    AudioFormat format;
-						    DataLine.Info info;
-						    Clip clip;
-						   
-						    stream = AudioSystem.getAudioInputStream(yourFile);
-						    System.err.println(stream );
-						    format = stream.getFormat();
-						    System.err.println(format );
-						    info = new DataLine.Info(Clip.class, format);
-						    
-						    clip = (Clip) AudioSystem.getLine(info);
-						    
-						    clip.open(stream);
-						    clip.start();
-						*/
 						}
 						catch (Exception e) {
+								JOptionPane.showMessageDialog(null,"error" + 
+						             ":\n" + e.getLocalizedMessage());
 							System.err.println(e);
-						}
-					  
-	  // Launching apps, i.e executing task ;-)
-					  Runtime runtime = Runtime.getRuntime();
-					  
-	  // runtime.exec(new String[] { "wmplayer //play //close K:\\mp3\\doorbell.mp3" } );
-	//  runtime.exec(new String[] { "C:\\Program Files\\Windows Media Player\\wmplayer K:\\mp3\\doorbell.mp3" } );
-	  //runtime.exec(new String[] { "play K:\\mp3\\doorbell.mp3" } );
-//					  runtime.exec(new String[] { "C:\\Users\\Loule\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe -profile-directory=\"Profile 1\""} ); // TO DO : define good user
-					  String url = "http://www.centerkey.com/";
+						}	
+					  // Launching browser
+					  String url = "http://www.wikipedia.com/";
 					  BrowserLauncher.openURL(url);
-					  
-					  //UDPClient answer = new UDPClient(String.valueOf(client),String.valueOf(client_port), "ok_received");
+
   	}
-  
-  
   }
-  catch(UnknownHostException ue){}
+  catch(UnknownHostException ue){					
+		JOptionPane.showMessageDialog(null,"error eerh" + 
+          ":\n" + ue.getLocalizedMessage());}
   }
   }
-  catch(java.net.BindException b){}
+  catch(java.net.BindException b){
+		JOptionPane.showMessageDialog(null,"error eeeee" + 
+	             ":\n" + b.getLocalizedMessage());
+  }
   }
   catch (IOException e){
+	  JOptionPane.showMessageDialog(null,"error aaaa" + 
+	             ":\n" + e.getLocalizedMessage());
   System.err.println(e);
   }
   }
